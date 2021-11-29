bd76a8eb257eeec8072439ef53c97857e18daad4
kawsarm104 <kawsarm104@gmail.com>
2021-11-30 01:52:09 +0600

    about me

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,13 +1,39 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import image from "../../image/personal/kimsoohyun.jpg";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <h1>About</h1>
-      <h1>About</h1>
-      <h1>About</h1>
-      <h1>About</h1>
+    <div className="" style={{ bacgroundColor: "#ffffff" }}>
+      <Container className="mt-3 mx-auto p-auto">
+        <Row className="">
+          <Col sm={12} md={5}>
+            <img src={image} className="img-fluid" alt="" />
+          </Col>
+          <Col sm={12} md={7}>
+            <h5 className="fw-bolder">ABOUT ME</h5>
+            <h2 className="fw-bolder my-4">Hello, I am Md. Kawsar Hossain.</h2>
+            <p className="mb-4">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim enim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <Row className="my-3">
+              <Col sm={12} md={6}>
+                <h6>Web Design</h6>
+                <h6>Web Design</h6>
+                <h6>Web Design</h6>
+              </Col>
+              <Col sm={12} md={6}>
+                <h6>Mobile Apps</h6>
+                <h6>Mobile Apps</h6>
+                <h6>Mobile Apps</h6>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import Typical from "react-typical";
+import image from "../../image/personal/kimsoohyun.jpg";
 import "./HomePage.css";
 
 const HomePage = () => {
@@ -7,14 +9,27 @@ const HomePage = () => {
     <>
       <Container fluid className="homepage">
         <Row className="mx-5">
-          <Col sm={12} md={7} className="">
-            <div className=" position-class text-center">
+          <Col sm={12} md={7} className="position-parent">
+            <div className=" position-child text-center bg-primary">
               <h1 className="">Md. Kawsar Hossain</h1>
               <h2>Front-End Developer</h2>
+              <div className="inline-class bg-warning">
+                <h3>I am a </h3>
+                <Typical
+                  steps={[
+                    "React Js Developer",
+                    1000,
+                    "Front-End Developer",
+                    500,
+                  ]}
+                  loop={Infinity}
+                  wrapper="h2"
+                />
+              </div>
             </div>
           </Col>
-          <Col sm={12} md={5} className="bg-info">
-            fdskkldskdsklklkfsklkl
+          <Col sm={12} md={5} className="p-3 mt-4">
+            <img src={image} className="img-fluid" alt="" />
           </Col>
         </Row>
       </Container>
