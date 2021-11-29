fe2ba80d2f1b60d5c3b780ed3dfafdd124b381c1
kawsarm104 <kawsarm104@gmail.com>
2021-11-29 11:07:52 +0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home/Home/Home';
+import AboutMe from './Pages/Home/AboutMe/AboutMe';
+import Education from './Pages/Home/Education/Education';
+import Portfolio from './Pages/Home/Portfolio/Portfolio';
+import Contact from './Pages/Home/Contact/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home></Home>
+      <AboutMe></AboutMe>
+      <Education></Education>
+      <Portfolio></Portfolio>
+      <Contact></Contact>
     </div>
   );
 }
