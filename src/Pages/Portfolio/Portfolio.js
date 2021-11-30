0576c56168ca818c5a4a6eca38318b45b04c615c
kawsarm104 <kawsarm104@gmail.com>
2021-11-30 17:34:31 +0600

    portfolio modified

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
-import ebike from "../../image/portfolio/screenshot-e-bike-client.web.app-2021.11.30-16_50_36.png"
-import kfood from "../../image/portfolio/screenshot-k-food-a0ea3.web.app-2021.11.30-16_51_06.png"
-import medicenter from "../../image/portfolio/screenshot-medicenter-dentist.firebaseapp.com-2021.11.30-16_51_42.png"
-import a7 from "../../image/portfolio/screenshot-serene-raman-b80cc4.netlify.app-2021.11.30-17_20_35.png"
-import a8 from "../../image/portfolio/screenshot-awesome-wozniak-716a19.netlify.app-2021.11.30-17_18_12.png"
-import a9 from "../../image/portfolio/screenshot-brave-mcnulty-9635a1.netlify.app-2021.11.30-17_19_27.png"
+import a8 from "../../image/portfolio/screenshot-awesome-wozniak-716a19.netlify.app-2021.11.30-17_18_12.png";
+import a9 from "../../image/portfolio/screenshot-brave-mcnulty-9635a1.netlify.app-2021.11.30-17_19_27.png";
+import ebike from "../../image/portfolio/screenshot-e-bike-client.web.app-2021.11.30-16_50_36.png";
+import kfood from "../../image/portfolio/screenshot-k-food-a0ea3.web.app-2021.11.30-16_51_06.png";
+import medicenter from "../../image/portfolio/screenshot-medicenter-dentist.firebaseapp.com-2021.11.30-16_51_42.png";
+import a7 from "../../image/portfolio/screenshot-serene-raman-b80cc4.netlify.app-2021.11.30-17_20_35.png";
 
 const Portfolio = () => {
   return (
